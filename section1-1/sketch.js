86ba6fb050d89933fe1acd98c24a842444bce65c
Takuma KAMIYAMA <takuma0727ns9hgreeeen@gmail.com>
2020-10-19 14:02:12 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,16 +1,17 @@
 function setup() {
-  createCanvas(128,128);
+  createCanvas(111,111);
 }
 
 function draw() {
   background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
+  strokeWeight(5);
+  fill(178, 90, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  triangle(0,104,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(62);
   textFont("serif");
   text("46", 68, 100);
 }
+ss
