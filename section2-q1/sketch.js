5165a47bfcf97abca7425c39e19f092b845d0be9
Takuma KAMIYAMA <takuma0727ns9hgreeeen@gmail.com>
2020-10-20 12:22:27 +0900

    小手調べ

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,11 @@
-// 小手調べ
++// 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+      // BLANK[1]
+      let x = i * 10 + 10;
+      ellipse(50,50,x);
+      if(i<5) {stroke(0,0,255);}
+      else{stroke(255,0,0);}
+    noFill();}
 }
+ss
